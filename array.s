292b71414e6b10a0c75e11dccb3a2909ae7003e5
Claudio <claudio.raimondi@protonmail.com>
2025-01-17 11:47:44 +0100

    Add array

--- array.s
+++ array.s
@@ -2,7 +2,7 @@
 
 .data
 arr_address: .byte 0x00000000
-n: .word 5
+n: .word 3
 
 
 .text
@@ -10,19 +10,17 @@ lb t0 arr_address
 lw s0 n
 
 li t1, 0 #i
+li t3, 0 #tmp
 li a0, 0 #ret
 
 loop:
     bgt t1, s0, stop #if i > n
     lbu t2, 0(t0) #t2 contiene arr[i]
-    beq t2, zero, count #if t2 == 0
-increment:
+    slti t3, t2, 1 #se e' minore di 1 e' per forza 0
+    add a0, a0, t3 #aggiungo al ret +1 o 0 a seconda se t3 == 1 o 0
     addi t1, t1, 1 #incrementa i
     addi t0, t0, 1 #incrementa arr
-    j loop    
-count:
-    addi a0, a0, 1
-    j increment
+    j loop
 
 stop:
 
