84d495128e9c9f2f7e3a9c99c2003ece5d1ebd43
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-01-20 20:35:09 +0100

    Optimize redditi_sospetti

--- redditi_sospetti.s
+++ redditi_sospetti.s
@@ -9,18 +9,16 @@ li a0, 0 #numero di redditi sospetti
 
 li s1, 64 #n
 start_loop:
-  beqz s1, end_loop       #if n == 0, end loop
-  lb t2, 0(t0)
-  sltiu t2, t2, 15000     # (array < 15000)
+  beqz s1, end_loop  #if n == 0, end loop
+  lbu t2, 0(t0)
+  sltiu t2, t2, 15   # (*array < 15) ovvero 15000$
   add a0, a0, t2
-  addi s1, s1, -1         #n--
-  addi t0, t0, 1          #array++
+  addi s1, s1, -1    #n--
+  addi t0, t0, 1     #array++
   j start_loop
 end_loop:
-  add t0, a0, 0       #tmp
-  slli t0, t0, 5      #res * 32
-  slli a0, a0, 2      #res * 4
-  add a0, a0, t0      #tot
+  mul a0, a0, 25     #si puo scomporre con 3 shift (2^4 + 2^3 + 2^0), ma la moltiplicazione e' gia' ben ottimizzata anche con algoritmo di booth
+  srli a0, a0, 4
 print:
   li a7, 1
   ecall
